4b81dbc53e86299755d25f24e54e8896066f45bc
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2022-06-12 21:57:55 +0200

    increase push version

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('health-check', function () {
     return response()->json([]);
 });
+
+
+Route::get('clear', function () {
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
+    \Illuminate\Support\Facades\Artisan::call('route:clear');
+    \Illuminate\Support\Facades\Artisan::call('optimize');
+});
