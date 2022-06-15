13d973be513be3153150c256972ae01e5ef661ec
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2022-06-15 16:10:12 +0200

    added darbi:reset command to run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,8 @@ cd /var/www
 
 #php artisan migrate
 #php artisan module:seed
-php artisan cache:clear
-php artisan route:cache
+#php artisan cache:clear
+#php artisan route:cache
+php artisan darbi:reset
 
 /usr/bin/supervisord -c /etc/supervisord.conf
