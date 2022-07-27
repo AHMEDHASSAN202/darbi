fce08d2953e161f939e3a77d947dafd4287d0bc0
muhammad awad <devmohamedawd@gmail.com>
2022-07-27 21:36:43 +0200

    style contact

--- Modules/CatalogModule/Resources/views/pages/contact.blade.php
+++ Modules/CatalogModule/Resources/views/pages/contact.blade.php
@@ -24,64 +24,70 @@
                     the below channels</p>
             </div>
             <div class="col-md-12">
-                <div class="card"
-                     style="border-radius: 12px;box-shadow: 0 10px 15px 0 rgba(43, 43, 43, 0.05);border: solid 1px #eaeaea;margin-bottom: 15px">
-                    <div class="card-body" style="padding: 15px 26px;">
-                        <div class="row" style="direction: ltr">
-                            <div class="col-3">
-                                <img src="{{asset('images/phone.png')}}" style="width: 50px;height: 50px" alt="">
-                            </div>
-                            <div class="col-9 p-0"
-                                 style="display: flex;flex-direction: column;justify-content: center;">
-                                <h4 style="margin:0;font-weight: 600;font-stretch: normal;font-style: normal;line-height: normal;letter-spacing: normal;text-align: left;color: #2b2b2b;">
-                                    Phone</h4>
-                                <p style="margin:0;font-size: 15px;font-weight: normal;font-stretch:normal;font-style:normal;line-height:normal;letter-spacing:normal;text-align:left;color:#757575;">
-                                    843 4234 243
-                                </p>
+                <a href="tel:843 4234 243">
+                    <div class="card"
+                         style="border-radius: 12px;box-shadow: 0 10px 15px 0 rgba(43, 43, 43, 0.05);border: solid 1px #eaeaea;margin-bottom: 15px">
+                        <div class="card-body" style="padding: 15px 26px;">
+                            <div class="row" style="direction: ltr">
+                                <div class="col-3">
+                                    <img src="{{asset('images/phone.png')}}" style="width: 50px;height: 50px" alt="">
+                                </div>
+                                <div class="col-9 p-0"
+                                     style="display: flex;flex-direction: column;justify-content: center;">
+                                    <h4 style="margin:0;font-weight: 600;font-stretch: normal;font-style: normal;line-height: normal;letter-spacing: normal;text-align: left;color: #2b2b2b;">
+                                        Phone</h4>
+                                    <p style="margin:0;font-size: 15px;font-weight: normal;font-stretch:normal;font-style:normal;line-height:normal;letter-spacing:normal;text-align:left;color:#757575;">
+                                        843 4234 243
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-md-12">
-                <div class="card"
-                     style="border-radius: 12px;box-shadow: 0 10px 15px 0 rgba(43, 43, 43, 0.05);border: solid 1px #eaeaea;margin-bottom: 15px">
-                    <div class="card-body" style="padding: 15px 26px;">
-                        <div class="row" style="direction: ltr">
-                            <div class="col-3">
-                                <img src="{{asset('images/whatsapp.png')}}" style="width: 50px;height: 50px" alt="">
-                            </div>
-                            <div class="col-9 p-0"
-                                 style="display: flex;flex-direction: column;justify-content: center;">
-                                <h4 style="margin:0;font-weight: 600;font-stretch: normal;font-style: normal;line-height: normal;letter-spacing: normal;text-align: left;color: #2b2b2b;">
-                                    Whatsapp</h4>
-                                <p style="margin:0;font-size: 15px;font-weight: normal;font-stretch:normal;font-style:normal;line-height:normal;letter-spacing:normal;text-align:left;color:#757575;">
-                                    +966 4234 243
-                                </p>
+                <a href="https://wa.com/+9664234243">
+                    <div class="card"
+                         style="border-radius: 12px;box-shadow: 0 10px 15px 0 rgba(43, 43, 43, 0.05);border: solid 1px #eaeaea;margin-bottom: 15px">
+                        <div class="card-body" style="padding: 15px 26px;">
+                            <div class="row" style="direction: ltr">
+                                <div class="col-3">
+                                    <img src="{{asset('images/whatsapp.png')}}" style="width: 50px;height: 50px" alt="">
+                                </div>
+                                <div class="col-9 p-0"
+                                     style="display: flex;flex-direction: column;justify-content: center;">
+                                    <h4 style="margin:0;font-weight: 600;font-stretch: normal;font-style: normal;line-height: normal;letter-spacing: normal;text-align: left;color: #2b2b2b;">
+                                        Whatsapp</h4>
+                                    <p style="margin:0;font-size: 15px;font-weight: normal;font-stretch:normal;font-style:normal;line-height:normal;letter-spacing:normal;text-align:left;color:#757575;">
+                                        +966 4234 243
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-md-12">
-                <div class="card"
-                     style="border-radius: 12px;box-shadow: 0 10px 15px 0 rgba(43, 43, 43, 0.05);border: solid 1px #eaeaea;margin-bottom: 15px">
-                    <div class="card-body" style="padding: 15px 26px;">
-                        <div class="row" style="direction: ltr">
-                            <div class="col-3">
-                                <img src="{{asset('images/twitter.png')}}" style="width: 50px;height: 50px" alt="">
-                            </div>
-                            <div class="col-9 p-0"
-                                 style="display: flex;flex-direction: column;justify-content: center;">
-                                <h4 style="margin:0;font-weight: 600;font-stretch: normal;font-style: normal;line-height: normal;letter-spacing: normal;text-align: left;color: #2b2b2b;">
-                                    Twitter</h4>
-                                <p style="margin:0;font-size: 15px;font-weight: normal;font-stretch:normal;font-style:normal;line-height:normal;letter-spacing:normal;text-align:left;color:#757575;">
-                                    @godarbi
-                                </p>
+                <a href="https://twitter.com/godarbi">
+                    <div class="card"
+                         style="border-radius: 12px;box-shadow: 0 10px 15px 0 rgba(43, 43, 43, 0.05);border: solid 1px #eaeaea;margin-bottom: 15px">
+                        <div class="card-body" style="padding: 15px 26px;">
+                            <div class="row" style="direction: ltr">
+                                <div class="col-3">
+                                    <img src="{{asset('images/twitter.png')}}" style="width: 50px;height: 50px" alt="">
+                                </div>
+                                <div class="col-9 p-0"
+                                     style="display: flex;flex-direction: column;justify-content: center;">
+                                    <h4 style="margin:0;font-weight: 600;font-stretch: normal;font-style: normal;line-height: normal;letter-spacing: normal;text-align: left;color: #2b2b2b;">
+                                        Twitter</h4>
+                                    <p style="margin:0;font-size: 15px;font-weight: normal;font-stretch:normal;font-style:normal;line-height:normal;letter-spacing:normal;text-align:left;color:#757575;">
+                                        @godarbi
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
             <div class="col-md-12">
                 <a href="mailto:info@darbi.com">
