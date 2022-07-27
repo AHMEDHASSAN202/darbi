fbee92371afadf9248f960b8f53567a69ca60394
muhammad awad <devmohamedawd@gmail.com>
2022-07-27 21:00:14 +0200

    style contact

--- Modules/CatalogModule/Resources/views/pages/contact.blade.php
+++ Modules/CatalogModule/Resources/views/pages/contact.blade.php
@@ -6,10 +6,12 @@
 
 
 @section('content')
-    <div class="container">
+    <div class="container pr-3 pl-3">
         <div class="row">
             <div class="col-md-12 mb-4">
-                <img src="{{asset('images/contact.png')}}" class="img-fluid w-100" alt="">
+                <div class="justify-content-center align-items-center p-3">
+                    <img src="{{asset('images/contact.png')}}" class="img-fluid w-100" alt="">
+                </div>
                 <p style="padding-top:10px;color: #888;text-align: center">Reach out to our support at the below channels</p>
             </div>
             <div class="col-md-12">
