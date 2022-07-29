05919382352ebe03e87f4d96a6fbfc39e211bbb0
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2022-07-29 20:13:44 +0200

    change push version

--- Modules/CommonModule/Services/InitService.php
+++ Modules/CommonModule/Services/InitService.php
@@ -43,7 +43,7 @@ class InitService
             'default_country'       => $this->settings->default_country,
             'default_city'          => $this->settings->default_city,
             'pages'                 => $this->pages(),
-            'push_version'          => 23,
+            'push_version'          => 24,
             'show_personal_ID'      => 0
         ];
     }
