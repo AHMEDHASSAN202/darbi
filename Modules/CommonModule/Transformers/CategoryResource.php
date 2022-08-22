b252bae65925ab16f0a91458f76408675fdfe3fa
AHMEDHASSAN202 <ahmedhassanhassan707@gmail.com>
2022-08-22 12:54:25 +0200

    optional settings value

--- Modules/CommonModule/Transformers/CategoryResource.php
+++ Modules/CommonModule/Transformers/CategoryResource.php
@@ -18,9 +18,7 @@ class CategoryResource extends JsonResource
             'name'              => translateAttribute($this['name']),
             'desc'              => translateAttribute($this['desc']),
             'image'             => imageUrl($this['image']),
-            'template'          => @$this['template'],
-            'webview'           => (bool)$this['webview'],
-            'webview_link'      => $this['webview']
+            'template'          => @$this['template']
         ];
     }
 }
